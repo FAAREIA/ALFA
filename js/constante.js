3c60799acbe50afb6a5c5a2bef2bfd3a9c58c4bd
FAAREIA <arena.federico@faareia.com>
2015-07-12 13:58:59 -0300

    Multiples fixes y agregados.

--- js/constante.js
+++ js/constante.js
@@ -5,7 +5,21 @@ $(document).ready(function() {
 	});
 
 	// Cerrar mensajes
-	$('.cerrar').on('click', function() {
-		$(this).parent().fadeOut(240);
+	$('.mensaje .cerrar').on('click', function() {
+		$(this).closest('.mensaje').fadeOut(240);
+	});
+
+	// Abrir emergentes
+	$('button[data-emergente]').on('click', function() {
+		var emergente = $(this).attr('data-emergente');
+		$('.emergente[data-emergente="' + emergente + '"]').fadeIn(480);
+	});
+
+	// Cerrar emergentes
+	$('.emergente').on('click', function(e) {
+		var tag = $(e.target);
+		if (tag.is(this) || tag.is('.cerrar')) {
+			$(this).fadeOut(240);
+		}
 	});
 });
