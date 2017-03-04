b51368253d368c6e91c3ae39af05f392b7d5d50d
FAAREIA <arena.federico@faareia.com>
2017-03-04 20:56:39 -0300

    ALFA v2.5.0
    Lots of changes...

--- js/tabs.js
+++ js/tabs.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 	// Set default height
-	var active_tab = $('.tabs section.active').innerHeight();
+	var active_tab = $('.tab.active').innerHeight();
 	$('div.tabs').css('height', active_tab);
 
 	// Navigate between tabs
 	$('button[data-tab]').on('click', function() {
 		var tab = $(this).attr('data-tab');
-		var tab_height = $('section[data-tab="' + tab + '"]').innerHeight();
 		$('nav.tabs button').removeClass('active');
 		$(this).addClass('active');
+		$('.tab[data-tab="' + tab + '"]').addClass('active').siblings().removeClass('active');
+		var tab_height = $('.tab[data-tab="' + tab + '"]').innerHeight();
 		$('div.tabs').css('height', tab_height);
-		$('section[data-tab="' + tab + '"]').addClass('active').siblings().removeClass('active');
 	});
 });
