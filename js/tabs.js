5d01bca701cc71d360a95b2e50e35c080b8c5d53
FAAREIA <arena.federico@faareia.com>
2018-04-01 21:57:59 -0300

    ALFA v2.11.0
    Minor fixes and improvements

--- js/tabs.js
+++ js/tabs.js
@@ -3,11 +3,13 @@ var active_tab = $('.tab-panel.fade-in').innerHeight();
 $('.tabs-content').css('height', active_tab);
 
 // Navigate between tabs
-$('button[data-tab]').on('click', function() {
-	var tab = $(this).attr('data-tab');
-	$('.tabs button').removeClass('active');
+$('[data-tab-button]').on('click', function() {
+	var tab = $(this).attr('data-tab-button');
+	var tab_panel_height = $('[data-tab-panel="' + tab + '"]').innerHeight();
+
+	$('[data-tab-button]').removeClass('active');
+	$('[data-tab-panel]').removeClass('fade-in');
 	$(this).addClass('active');
-	$('.tab-panel[data-tab="' + tab + '"]').addClass('fade-in').siblings().removeClass('fade-in');
-	var tab_height = $('.tab-panel[data-tab="' + tab + '"]').innerHeight();
-	$('.tabs-content').css('height', tab_height);
+	$('[data-tab-panel="' + tab + '"]').addClass('fade-in');
+	$('.tabs-content').css('height', tab_panel_height);
 });
